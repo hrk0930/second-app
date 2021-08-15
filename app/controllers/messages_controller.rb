06427938f3183ac8690c7910dc0db6109e898167
hrk0930 <a08062151874@icloud.com>
2021-08-15 15:10:43 +0900

    メッセージ投稿機能　完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @messages = Message.all
+    @messages = Message.order("created_at DESC")
   end
+
+  def create
+    @message = Message.new(content_params)
+     if@message.save
+      redirect_to messages_path
+     else
+      render :index
+     end
+  end
+
+  def destroy
+    messages = message.find(params[:id])
+    message.content.destroy
+    redirect_to root_path
+  end
+
+  private
+  def content_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
 end
