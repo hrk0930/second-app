bddb53c1875e43302df3bba0b2b00f32bc2574a6
hrk0930 <a08062151874@icloud.com>
2021-08-24 17:06:51 +0900

    編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :ssh_options, verify_host_key: :secure
 
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano (3.16.0)'
+lock '3.16.0'
 
 # Capistranoのログの表示に利用する
 set :application, 'second-app'
@@ -51,7 +51,7 @@ set :repo_url,  'git@github.com:hrk0930/second-app.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '’2.6.5’' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
